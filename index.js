4233215eba56305e0cc859e8b4dbc1f1017db28b
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-20 02:02:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function happyHolidays () {
 function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo (name) {
+  var holiday = Christmas, 
+  var name = Rebecca,
+  return `Happy ${holiday}, ${name}!`
+}
